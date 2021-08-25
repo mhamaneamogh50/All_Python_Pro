93673a12022fdd9172d85c4a7648c7b2dcf2f112
Amogh Mhamane <88707054+mhamaneamogh50@users.noreply.github.com>
2021-08-25 12:57:44 +0530

    File_operations_ITR_Assig
    ITR

--- fileoperation.py
+++ fileoperation.py
@@ -1,13 +1,14 @@
-# Reading file
-##f = open('text.txt','r')
-##print(f.read())
-###Writing in file
-##f = open("text.txt", "w")
-##f.write("now wrote in file")
-##f.close()
-##f = open("text.txt", "r")
-##print(f.read())
+#Writing in file 
+with open("text.txt", "w",encoding='utf-8')as f:
+    f.write("now wrote in file")
+    f.close()
+    f = open("text.txt", "r")
+print(f.read())
+##Reading file
+f = open('text.txt','r',encoding='utf-8')
+print(f.read())
 # closing opened file
-f = open("text.txt", "r")
+f = open("text.txt", "r",encoding='utf-8')
 print(f.read())
 f.close()
+    
