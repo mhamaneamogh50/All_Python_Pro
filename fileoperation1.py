97849c8877e652f49b3565785cbfb6b89d86bf0a
Amogh Mhamane <88707054+mhamaneamogh50@users.noreply.github.com>
2021-08-25 12:49:15 +0530

    File_operations

---
+++ fileoperation1.py
@@ -0,0 +1,14 @@
+##Reading file
+f = open('text.txt','r')
+print(f.read())
+#Writing in file
+f = open("text.txt", "w")
+f.write("now wrote in file")
+f.close()
+f = open("text.txt", "r")
+print(f.read())
+# closing opened file
+f = open("text.txt", "r")
+print(f.read())
+f.close()
+
